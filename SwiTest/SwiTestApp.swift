04b0890cb513c25decc5bcc5b687172c6aada701
moolnam <moolnam>
2022-05-30 22:55:28 +0900

    Initial Commit

---
+++ SwiTest/SwiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiTestApp.swift
+//  SwiTest
+//
+//  Created by KimJongHee on 2022/05/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
